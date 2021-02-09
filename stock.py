3fc309202181801d513b15adc2b988e217867a78
Logesh Varman <58332339+lokeshvarman12@users.noreply.github.com>
2021-02-09 20:54:02 +0530

    Add files via upload

---
+++ stock.py
@@ -0,0 +1,5 @@
+import yfinance as yf
+#for example here I'm using FB(facebook) as a searching stock name
+stock = yf.Ticker("zoho")
+data = stock.history (period="Id",start ="2021-1-1",end ="2021-1-29")
+print(data)

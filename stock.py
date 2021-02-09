592f9eb71b2686ed3ff1d5c10f1b11c839d9bf7e
Logesh Varman <58332339+lokeshvarman12@users.noreply.github.com>
2021-02-09 20:54:28 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,5 +1,5 @@
 import yfinance as yf
 #for example here I'm using FB(facebook) as a searching stock name
-stock = yf.Ticker("zoho")
+stock = yf.Ticker("FB")
 data = stock.history (period="Id",start ="2021-1-1",end ="2021-1-29")
 print(data)
